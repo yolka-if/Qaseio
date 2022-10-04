24c066bd44b4db22d3acd22cb5327602d3987358
yolka_if <375293572290@mail.ru>
2022-10-04 19:33:46 +0300

    Правки после ревью

--- src/test/java/pages/ProjectsPage.java
+++ src/test/java/pages/ProjectsPage.java
@@ -1,8 +1,6 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
@@ -25,22 +23,24 @@ public class ProjectsPage {
 
     public void isProjectExist(String projectName){
         open("https://app.qase.io/projects");
-        $(byXpath(String.format("//tbody//a[contains(text(), '%s')]",projectName))).shouldBe(Condition.visible);
+        $(byXpath(String.format("//tbody//a[contains(text(), '%s')]", projectName))).shouldBe(Condition.visible);
     }
 
-    public void deleteProject(String projectName){
+    public void deleteProject(String projectName) {
         open("https://app.qase.io/projects");
-        $(byXpath(String.format("//a[text()='%s']",projectName))).click();
+        $(byXpath(String.format("//a[text()='%s']", projectName))).click();
         $(byXpath("//span[text()='Settings']")).click();
         $(byXpath("//*[contains(text(), 'Delete project')]")).click();
         $(byXpath("//*[contains(text(), 'Delete project')]")).click();
     }
 
-    public void renameProject(){
-      //  open("https://app.qase.io/projects");
-        $(byXpath("//span[text()='Settings']")).click();
-        $("#inputTitle").setValue("Переименован");
-        $("#update").click();
+    public void projectNotExist(String projectName) {
+        $(byXpath(String.format("//a[text()='%s']", projectName))).shouldNotBe(Condition.visible);
+    }
 
+    public void renameProject(String newName) {
+        $(byXpath("//span[text()='Settings']")).click();
+        $("#inputTitle").setValue(newName);
+        $("#update").click();
     }
 }
--- src/test/java/tests/ProjectTest.java
+++ src/test/java/tests/ProjectTest.java
@@ -1,43 +1,44 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
-
-import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Selenide.$;
 
 
 public class ProjectTest extends BaseTest {
     Faker fakerName = new Faker();
     Faker fakerCode = new Faker();
-    String projectName = fakerName.funnyName().name();
-    String code = fakerCode.color().name();
 
     @Test
     public void createProject() {
+        String projectName = fakerName.funnyName().name();
+        String code = fakerCode.color().name();
         loginPage.openPage();
         loginPage.login(login, password);
         projectsPage.createProject(projectName, "Test", code);
-        projectsPage.isProjectExist(projectName);
         projectsPage.isProjectExist(projectName);
     }
 
     @Test
     public void deletePtoject() {
+        String projectName = fakerName.funnyName().name();
+        String code = fakerCode.color().name();
         loginPage.openPage();
         loginPage.login(login, password);
         projectsPage.createProject(projectName, "Test", code);
         projectsPage.deleteProject(projectName);
-        $(byXpath(String.format("//a[text()='%s']", projectName))).shouldNotBe(Condition.visible);
+        projectsPage.projectNotExist(projectName);
+
     }
 
     @Test
     public void renameProject() {
+        String projectName = fakerName.funnyName().name();
+        String code = fakerCode.color().name();
+        String newName = "Переименован";
         loginPage.openPage();
         loginPage.login(login, password);
         projectsPage.createProject(projectName, "Test", code);
-        projectsPage.renameProject();
-        projectsPage.isProjectExist("Переименован");
+        projectsPage.renameProject(newName);
+        projectsPage.isProjectExist(newName);
     }
 }
